017262f7b95e3cae01e64eb745abf25e4e48423e
ottozip <ottozipx@gmail.com>
2025-02-16 21:02:20 +0900

    [Bronze III] Title: 알람 시계, Time: 128 ms, Memory: 16264 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if (M >= 45) {
+            M -= 45;
+        } else {
+            if (H == 0) {
+                H = 24;
+            }
+
+            H -= 1;
+            M += 15;
+        }
+
+        System.out.println(H + " " + M);
+    }
+}
